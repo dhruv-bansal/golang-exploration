16f81918c3c2c15aea23b0d9a8adfef66729cd79
dhruv-bansal <dhruv.bansal@outlook.com>
2020-08-05 15:27:39 +0530

    complex data type

--- basic/data-types/main.go
+++ basic/data-types/main.go
@@ -17,4 +17,16 @@ func main() {
 	title := "go data types"
 	fmt.Println("Title", title)
 
+	b := false
+	fmt.Println("Boolean", b)
+
+	// complex data type is built in GO
+	c := complex(3, 4)
+	fmt.Println("Complex Variable", c)
+
+	// multi assignment in the go
+	// two variables in the left and two functions in the right
+	real, img := real(c), imag(c)
+	fmt.Println("Real number and imaginary number", real, img)
+
 }
