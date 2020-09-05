f25b7d9a371912de0b2ce753843271dbdb22559c
dhruv-bansal <dhruv.bansal@outlook.com>
2020-09-05 08:50:10 +0530

    adding example of init

---
+++ basic/init/init.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// Multiple init() function
+func init() {
+	fmt.Println("Welcome to init() function")
+}
+
+func init() {
+	fmt.Println("Hello! init() function")
+}
+
+func main() {
+
+	fmt.Println("Welcome to main() function")
+}
